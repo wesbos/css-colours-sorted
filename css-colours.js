65bc6327774c620fb9f3b73514eddafc29b9b1f8
Wes Bos <wesbos@gmail.com>
2019-09-10 12:34:10 -0400

    Neeeeat

---
+++ css-colours.js
@@ -0,0 +1,37 @@
+import { colorsByLength, isValidColor, isDark, colorsByHue, colorsBySaturation, colorsByLightness, colorsByAll, colorsByVowels } from './colors';
+
+function displayColoursBy(colorsToDisplay, el, toShow = 'hsl') {
+  const spans = colorsToDisplay
+    .map(
+      color =>
+        `<span class="color ${color.name} ${
+        color.isDark ? 'dark' : ''
+        }" style="background: ${color.name};">${color.name.toUpperCase()}<br>${color[toShow]}</span>`
+    )
+    .join('');
+  el.innerHTML = spans;
+}
+
+function displayGradient(colorsToDisplay, el, toShow = 'hsl') {
+  const spans = colorsToDisplay
+    .map(
+      (color, i) => {
+        const lastColor = i > 0 ? colorsToDisplay[i - 1] : color;
+       return `<span class="color ${color.name} ${
+          color.isDark ? 'dark' : ''
+        }" style="background: linear-gradient(to right, ${lastColor.hex} 0%,${color.hex} 100%);">${color.name.toUpperCase()}<br>${color[toShow].join('/')}</span>`
+      }
+        )
+    .join('');
+  el.innerHTML = spans;
+}
+
+// displayColours();
+displayColoursBy(colorsByLength, document.querySelector('.by-length'));
+displayColoursBy(colorsByHue, document.querySelector('.by-hue'));
+displayColoursBy(colorsBySaturation, document.querySelector('.by-saturation'));
+displayColoursBy(colorsByLightness, document.querySelector('.by-lightness'));
+displayColoursBy(colorsByAll, document.querySelector('.by-all'));
+displayGradient(colorsByVowels, document.querySelector('.by-gradient'));
+displayColoursBy(colorsByVowels, document.querySelector('.by-vowels'), 'vowels');
+
---
+++ readme.md
@@ -0,0 +1,8 @@
+# CSS Colours Sorted By ____
+
+This is a fun way to trick you into learning JavaScript's map/filter/reduce along with some other neat things.
+
+Clone it, then `npm install`, then `npm start`.
+
+![](https://wes.io/7814d5756242/content)
+
